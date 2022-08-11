ec6fc68f2805f7dea583dce4bf6160909ad4a525
ig1na <greenig1na@gmail.com>
2022-08-11 22:44:14 +0200

    remove project files

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave:false,
-  devServer: {
- proxy: 'https://api.clashroyale.com/v1/tournaments'
-}
-})
